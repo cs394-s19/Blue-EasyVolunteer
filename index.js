720ab87d1527b7587c90b5632ee85d8cc0a63454
Justin Chen <justinfc42@gmail.com>
2019-04-07 17:50:27 -0500

    added getCalendar

--- index.js
+++ index.js
@@ -21,20 +21,39 @@ app.post('/userSubmit', (req, res) => {
   });
 });
 
+// getCalendar: Returns all occupied time slots for an event
 app.get('/getCalendar/:id', (req, res) => {
-  let sql = "GET lkjf from events where eventID = ?"
+  // SQL Query to obtain every row related to a specific event ID
+  let sql = "SELECT * FROM events WHERE eventID = ?"
   connection.query(sql, [req.params.id], (error, result) => {
+    // If it fucks up, return error code 500
     if (error) res.sendStatus(500);
     else {
-      res.json({calendar : result['columname']});
+      // Initialize empty 2d array of 31 days x 24 hours of 0's
+      // sets up the array for adding actual days/times for filled timeslots
+      var calendarInfo = [[]];
+      for (i = 0; i < 31; i++) {
+        for (j = 0; j < 24; j++) {
+          calendarInfo[i].push(0)
+        }
+        calendarInfo.push([])
+      }
+
+      // For each eventID-related row, fill calendarInfo with the legitimately filled timeslots
+      for (i = 0; i < result.length; i++) {
+        calendarInfo[result[i][dayIndex]], result[i][timeIndex]] = result[i][volunteerID]
+      }
+
+      // Return calendarInfo
+      res.json({calendar : calendarInfo});
     }
   });
 });
 
-// Gets the volunteer name from the volunteer table 
+// Gets the volunteer name from the volunteer table
 app.get('/getVolunteerName/:volunteerID', (req, res) => {
   // SQL statement for getting volunteer name
-  let sql = "SELECT volunteerName from volunteer where volunteerID = ?;"
+  let sql = "SELECT volunteerName FROM volunteer WHERE volunteerID = ?;"
   // Query for volunteerID
   connection.query(sql, [req.params.volunteerID], (error, result) => {
     if (error) {
