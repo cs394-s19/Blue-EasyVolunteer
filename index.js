9dd7e6b1f62826b2cea9f5286e87e071ea097fa9
Kenneth Gomez <kennethgomez2020@u.northwestern.edu>
2019-04-07 17:55:01 -0500

    made user submitting route. merging with justin

--- index.js
+++ index.js
@@ -13,7 +13,36 @@ const connection = mysql.createConnection({
 
 
 app.post('/userSubmit', (req, res) => {
+  // inserts basic info for the volunteer.
   let userInfoQuery = "INSERT INTO volunteer (volunteerName, phone, email) VALUES (?, ?, ?)";
+
+  let userBlockQuery = "INSERT INTO block (eventID, dayIndex, timeIndex, volunteerID) VALUES (?, ?, ?, ?)";
+  // gets the user id for the newly made user
+  let userIDQuery = "SELECT COUNT(*) volunteer";
+
+  // req.body.calendar is a 2D array indexed by date and time
+  let calendar = req.body.calendar;
+
+
+  // gets the total number of days and timeslots from the calendar
+  let totalDays = calendar.length;
+  let totalTimes = calendar[0].length;
+
+  // first grabs the userID with the count query, then populates the block table
+  connection.query(userIDQuery, [], (error, result) => {
+    if (error) res.sendStatus(500);
+    else {
+      for (let ii = 0; ii<totalDays; ii++) {
+        for (let jj = 0; jj<totalTimes; jj++) {
+          if (calendar[ii][jj]) {
+            connection.query(userBlockQuery, [res.body.eventID, ii, jj, result], (error, result2) => {
+              if (error) res.sendStatus(500);
+            });
+          }
+        }
+      }
+    }
+  });
   
   connection.query(userInfoQuery, [req.body.name, req.body.phone, req.body.email], (error, result) => {
     if (error) res.sendStatus(500);
@@ -23,7 +52,7 @@ app.post('/userSubmit', (req, res) => {
 
 app.get('/getCalendar/:id', (req, res) => {
   let sql = "GET lkjf from events where eventID = ?"
-  connection.query(sql, [req.param.id] (error, result) => {
+  connection.query(sql, [req.param.id], (error, result) => {
     if (error) res.sendStatus(500);
     else {
       res.json({calendar : result['columname']});
