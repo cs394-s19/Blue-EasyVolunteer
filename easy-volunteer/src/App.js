2c9d6137123b94a2fc9124ba338623ae4b414497
Shreyas Tallamraju <shreyastallamraju2020@u.northwestern.edu>
2019-04-07 22:53:47 -0500

    edited for backend implementation

--- easy-volunteer/src/App.js
+++ easy-volunteer/src/App.js
@@ -2,37 +2,36 @@ import React from 'react';
 import './App.css';
 
 //load calendar
-loadCalendar();
-let testCalendarObject = [
-  [0], //day 0 - monday
-  [1],
-  [0],
-  [2],
-  [0]
-];
-
-let globalCalendar;
+let globalCalendar = loadCalendar();
 
 function loadCalendar() {
   //pull calendar from database and put to globalCalendar
+  //parameters: none
+  //result: 2D calendar from database
 }
 
-function schedule(inName, dayIndex) {
+function schedule(inName, dayIndex, slots) {
+  //name is name, dayIndex is monday/tuesday/wednesday, slots is an array of 
+}
 
+function getDayIndex(id) {
+  if (id === "M") {
+    return 0;
+  } else if (id === "Tu") {
+    return 1;
+  } else if (id === "W") {
+    return 2;
+  } else if (id === "Th") {
+    return 3;
+  } else if (id === "F") {
+    return 4;
+  } else {
+    return -1;
+  }
 }
 
 function getVolunteerFromID(id) {
-  //to be implemented
-  if(id === 1){
-    return "Joseph";
-  }
-  else if (id === 2){
-    return "Paul";
-  }
-  else if (id === 0){
-    return "";
-  }
-  return "Other";
+  //get volunteer ID, return volunteer name
 } 
 
 let username = "";
@@ -51,6 +50,7 @@ const Day = ({day, busy, name}) => {
         setDynamicName(username);
         busy = !busy;
         setBusySetting(String(busy));
+        schedule(username, getDayIndex(day), [0]);
       }
     } else { //if busy
       if(username === ""){
@@ -63,6 +63,7 @@ const Day = ({day, busy, name}) => {
           if (busy === false){
             setDynamicName("");
           }
+          schedule(username, getDayIndex(day), [0]);
         } else {
           return;
         }
@@ -90,11 +91,11 @@ const Calendar = () => (
   <table className="calendar">
     <tbody>
     <tr>
-      <th><Day day="M" busy={inferBusy(testCalendarObject[0][0])} name={getVolunteerFromID(testCalendarObject[0][0])}></Day></th>
-      <th><Day day="Tu" busy={inferBusy(testCalendarObject[1][0])} name={getVolunteerFromID(testCalendarObject[1][0])}></Day></th>
-      <th><Day day="W" busy={inferBusy(testCalendarObject[2][0])} name={getVolunteerFromID(testCalendarObject[2][0])}></Day></th>
-      <th><Day day="Th" busy={inferBusy(testCalendarObject[3][0])} name={getVolunteerFromID(testCalendarObject[3][0])}></Day></th>
-      <th><Day day="F" busy={inferBusy(testCalendarObject[4][0])} name={getVolunteerFromID(testCalendarObject[4][0])}></Day></th>
+      <th><Day day="M" busy={inferBusy(globalCalendar[0][0])} name={getVolunteerFromID(globalCalendar[0][0])}></Day></th>
+      <th><Day day="Tu" busy={inferBusy(globalCalendar[1][0])} name={getVolunteerFromID(globalCalendar[1][0])}></Day></th>
+      <th><Day day="W" busy={inferBusy(globalCalendar[2][0])} name={getVolunteerFromID(globalCalendar[2][0])}></Day></th>
+      <th><Day day="Th" busy={inferBusy(globalCalendar[3][0])} name={getVolunteerFromID(globalCalendar[3][0])}></Day></th>
+      <th><Day day="F" busy={inferBusy(globalCalendar[4][0])} name={getVolunteerFromID(globalCalendar[4][0])}></Day></th>
     </tr>
     </tbody>
   </table>
