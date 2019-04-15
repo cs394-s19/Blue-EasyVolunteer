f03435456f184a96533eee0e51a00b787b7c605a
Jair Rojas <Jair>
2019-04-14 21:00:44 -0500

    updated frontend

--- easy-volunteer/src/App.js
+++ easy-volunteer/src/App.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import logo from './assets/logo2.svg'
-import './semantic/dist/semantic.min.css';
 import './App.css';
 
 const axios = require('axios');
@@ -14,7 +12,7 @@ const axios = require('axios');
 // }
 
 // function schedule(inName, dayIndex, slots) {
-//   //name is name, dayIndex is monday/tuesday/wednesday, slots is an array of
+//   //name is name, dayIndex is monday/tuesday/wednesday, slots is an array of 
 // }
 
 function getDayIndex(id) {
@@ -52,179 +50,64 @@ function getVolunteerFromID(id) {
 
   if (id !== 0)
     return id;
-}
+} 
 
 let username = "";
 
-const Day = ({day, busy, name}) => {
-  const originalName = name;
-  const [busySetting, setBusySetting] = React.useState(String(busy));
-  const [dynamicName, setDynamicName] = React.useState(originalName);
-
-  function displayName(dynName,name)
-  {
-    if(dynName === 0)
-    {
-      if (name === 0){
-        return "";
-      }
-      return name;
+const Slot = ({userID}) => {
+  const [busySetting, setBusySetting] = React.useState(String(inferBusy(userID)));
+  const [uID, setuID] = React.useState(String(userID));
+  function handleSlotClick(){
+    // call Firebase to assign user id
+    if(busySetting === "true"){
+      setBusySetting("false");
     }
-    else
-    {
-      return dynName;
+    else{
+      setBusySetting("true"); 
     }
+    console.log("hey");
   }
+  return(
+    <div className={"slot-" + busySetting} onClick={handleSlotClick}>
+      {uID}
+    </div>
+  );
+}
 
+const Day = ({ids, header}) => {
+  const slots = ids.map((item) => <Slot userID={item}></Slot>)
+  return(
+    <div className="day">
+      <div className="header">
+        {header}
+      </div>
 
-  function handleDayClick() {
-    if(busySetting === "false"){ //if not busy
-      if(username === ""){ //if not logged in
-        alert("You are not logged in!");
-        return;
-      }
-      else { //if logged in
-        setDynamicName(username);
-        busy = !busy;
-        setBusySetting(String(busy));
-
-        ////////// start of backend part ////////////////
-        //console.log(username);
-        axios.get('http://localhost:4200/getVolunteerID/' + username, {
-          headers: {
-            'Content-type': 'application/json'
-          }
-        })
-        .then( (response) => {
-          axios.post('http://localhost:4200/updateSchedule', {
-            volunteerID: response.data.volunteerID,
-            eventID: 1,
-            times: [true],
-            dayIndex: getDayIndex(day),
-            headers: {
-              'Content-type': 'application/x-www-form-urlencoded'
-            }
-          })
-          .then(function (response) {
-            console.log("database updated");
-          })
-          .catch(function (error) {
-            console.log("error in updateSchedule: " + error);
-          });
-        })
-        .catch ( (error) => {
-          console.log("error in getVolunteerID: " + error);
-        });
-
-        //////////////// end of backend part ////////////////
-      }
-    } else { //if busy
-      if(username === ""){
-        return;
-      }
-      else {
-        if (dynamicName === username) {
-          busy = !busy;
-          setBusySetting(String(busy));
-          if (busy === false) {
-            setDynamicName("");
-          }
-
-          ////////// start of backend part ////////////////
-          axios.get('http://localhost:4200/getVolunteerID/' + dynamicName, {
-            headers: {
-              'Content-type': 'application/json'
-            }
-          })
-          .then( (response) => {
-            axios.post('http://localhost:4200/updateSchedule', {
-              volunteerID: response.data.volunteerID,
-              eventID: 1,
-              times: [false],
-              dayIndex: getDayIndex(day),
-              headers: {
-                'Content-type': 'application/x-www-form-urlencoded'
-              }
-            })
-            .then(function (response) {
-              console.log("database updated");
-            })
-            .catch(function (error) {
-              console.log("error in updateSchedule: " + error);
-            });
-          })
-          .catch ( (error) => {
-            console.log("error in getVolunteerID: " + error);
-          });
-
-          //////////////// end of backend part ////////////////
-        } else {
-          return;
-        }
-      }
-    }
-  }
-  return (
-   <table className="day"><tbody>
-    <tr>
-      <th className="day-header">{day}</th>
-    </tr>
-    <tr>
-      <td onClick={handleDayClick} className={busySetting}>{displayName(dynamicName,name)}</td>
-    </tr>
-    </tbody>
-  </table>
+      <div className="slots">
+       {slots}
+      </div>
+    </div>
   );
 }
 
 function inferBusy(id) {
   console.log(id);
-  return (id === 0) ? false : true;
+  return (id === 0) ? false : true; 
 }
 
 const Calendar = () => {
-  let initialCalendar = [[0],[0],[0],[0],[0]];
-  const [calendar, setCalendar] = useState(initialCalendar);
-
-  //////// backend route ////////////////////////
-  useEffect(() => {
-    axios.get('http://localhost:4200/getCalendar/1', {
-      headers: {
-        'Content-type': 'application/json; charset=utf-8'
-      }
-    })
-      .then( (response) => {
-        // handle success
-        console.log(response.data.calendar); // the 2d calendar array, each value is a volunteerID
-        setCalendar(response.data.calendar);
-        console.log(calendar);
-
-      })
-      .catch(function (error) {
-        // handle error
-        console.log("error in getCalendar" + error);
-      });
-  }, []);
-
-  function getUserIDs()
-  {
-
-  }
-
-  /////////////////// end back-end route ///////////////////////////
-  return (
-  <table className="calendar">
-    <tbody>
-    <tr>
-      <th><Day day="M" busy={inferBusy(calendar[0][0])} name={calendar[0][0]}></Day></th>
-      <th><Day day="Tu" busy={inferBusy(calendar[1][0])} name={calendar[1][0]}></Day></th>
-      <th><Day day="W" busy={inferBusy(calendar[2][0])} name={calendar[2][0]}></Day></th>
-      <th><Day day="Th" busy={inferBusy(calendar[3][0])} name={calendar[3][0]}></Day></th>
-      <th><Day day="F" busy={inferBusy(calendar[4][0])} name={calendar[4][0]}></Day></th>
-    </tr>
-    </tbody>
-  </table>
-  )
+  const headers = ["Mon", "Tues", "Wed", "Thur", "Fri"];
+  const eventName = "Sample Event";
+  const db = [[0, 1],[0, 0],[1, 0],[0, 0],[1, 1]];
+  const numDays = db.length;
+  const numSlots = db[0].length;
+  const days = db.map((day) =>
+      <Day ids={day} header="day"></Day>
+    );
+  return(
+    <div className="calendar">
+      {days}
+    </div>
+  );
 };
 
 const Login = () => {
@@ -250,19 +133,18 @@ const Login = () => {
   }
   return (
     <div>
-      <form class="form">
-        <input class="ui icon input login" id="login" type="text" placeholder="Login here..." />
-        <button class="ui primary button login" onClick={handleLogin} type="button">Login</button>
-        <LoginInfo name={name}/>
+      <form>
+        <input id="login" type="text" placeholder="Login here..." /><br />
+        <button onClick={handleLogin} type="button">Login</button>
       </form>
-
+      <LoginInfo name={name}/>
     </div>
   );
 }
 
 const LoginInfo = ({name}) => ( ((name !== "") && (name != null)) ?
   <p>
-    Signed in as: {name}
+    Signed in as: <br /> {name}
   </p>
   : <p></p>
 );
@@ -272,21 +154,19 @@ const App = () => {
       <center>
       <br /><br />
       <div className="App">
-
-        <div className="logoDiv">
-          <img className="logo" src={logo} alt="logo" />
-        </div>
-
-        <div className="calendarDiv">
-          <Calendar className="calendar" />
-        </div>
-
-        <div className="loginDiv">
-          <Login />
-
-
-        </div>
-
+        <table className="main-table">
+        <tbody>
+        <tr>
+          <th>
+            <div className="left-sidebar">
+              <div className="logo"><h1>EasyVolunteer</h1></div>
+              <Login />
+            </div>
+          </th>
+          <th className="right-sidebar"><Calendar className="calendar" /></th>
+        </tr>
+      </tbody>
+      </table>
       </div>
       </center>
     )
