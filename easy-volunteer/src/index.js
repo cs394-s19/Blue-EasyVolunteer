f319e0f14118945e58b7e72f6931b7b8b0dab6eb
ZacharyRMcKee <zmckee@hawk.iit.edu>
2019-04-15 23:13:01 -0500

    I made a really crappy home page

--- easy-volunteer/src/index.js
+++ easy-volunteer/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Home from './Home';
 import 'semantic-ui-css/semantic.min.css'
 import EventForm from './components/CreateEvent'
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
@@ -12,6 +13,7 @@ const routing = (
     <div>
       <Route path="/:id" component={App} />
       <Route path="/newEvent" component={EventForm} />
+      <Route exact path="/" component={Home} />
     </div>
   </Router>
 )
