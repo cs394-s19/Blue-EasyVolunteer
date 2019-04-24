ebebc3dae1db13a6166a977ff5cee74a0e63db1d
VictorAung <VictorAung2021@u.northwestern.edu>
2019-04-24 14:23:30 -0500

    made home page a little better

--- easy-volunteer/src/Home.js
+++ easy-volunteer/src/Home.js
@@ -3,26 +3,59 @@ import React, { useState, useEffect } from 'react';
 import { firebase } from './firebaseConfig';
 import { Form, Button } from 'semantic-ui-react';
 import './App.css';
+import logo from './assets/logo2.svg'
 
 
 const Home = () => {
-    return (<div class="wrapper">
+    return (
+      <center>
+      <br /><br />
+      <div className="App">
                 <Header />
                 <CreateEventPrompt />
-            </div>);
+      </div>
+        </center>
+
+          );
 };
 const Header = () => {
-    return  <div className="logo"><h1>EasyVolunteer</h1></div>
+    return  (
+      <div className="logoDiv">
+        <img className="logo" src={logo} alt="logo" />
+      </div>
+);
 };
 const CreateEventPrompt = () => {
-    const HandleEventClick = () => 
+    const HandleEventClick = () =>
     {
         window.location = "/newEvent";
     }
-    return (<div class="EventPrompt">
-                <div id="Create-Event" onClick={() => HandleEventClick()}>
-                Create A New Event
+    return (
+      <div class="EventPrompt">
+        <div>
+              Welcome to EasyVolunteer - So easy, it's Volunteezy.
+        </div>
+
+        <div class="instructions">
+              EasyVolunteer is an app that provides a quick and simple way to let volunteers sign up for shifts.
                 </div>
-            </div>);
+        <div>
+        To get started, click on the button below.
+
+        </div>
+            <div class="newEvent">
+                <Form onSubmit={() => HandleEventClick()}>
+                <Form.Group>
+                  <Button primary type='submit' >Create a New Event </Button>
+                </Form.Group>
+                </Form>
+            </div>
+            </div>
+
+
+          //
+
+
+          );
 };
 export default Home;
